8cbfbc2b1d9f78322f93c5315a2248a6bc252313
Matin35 <matin.35m@gmail.com>
2025-02-07 15:22:30 +0330

    header desktop size(not responsive)

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const newMenu = document.getElementById("new")
+const newSubMenu = document.getElementById("newSubMenu")
+newMenu.addEventListener("mouseover", ()=>{
+    newSubMenu.setAttribute("class","newSubMenu newHover")
+})
+newSubMenu.addEventListener("mouseover", ()=>{
+    newSubMenu.setAttribute("class","newSubMenu newHover")
+})
+addEventListener("mouseout", ()=>{
+    newSubMenu.setAttribute("class","newSubMenu")
+})
+const menMenu = document.getElementById("men")
+const menSubMenu = document.getElementById("menSubMenu")
+menMenu.addEventListener("mouseover", ()=>{
+    menSubMenu.setAttribute("class","menSubMenu menHover")
+})
+menSubMenu.addEventListener("mouseover", ()=>{
+    menSubMenu.setAttribute("class","menSubMenu menHover")
+})
+addEventListener("mouseout", ()=>{
+    menSubMenu.setAttribute("class","menSubMenu")
+})
+const womenMenu = document.getElementById("women")
+const womenSubMenu = document.getElementById("womenSubMenu")
+womenMenu.addEventListener("mouseover", ()=>{
+    womenSubMenu.setAttribute("class","womenSubMenu womenHover")
+})
+womenSubMenu.addEventListener("mouseover", ()=>{
+    womenSubMenu.setAttribute("class","womenSubMenu womenHover")
+})
+addEventListener("mouseout", ()=>{
+    womenSubMenu.setAttribute("class","womenSubMenu")
+})
+const kidsMenu = document.getElementById("kids")
+const kidsSubMenu = document.getElementById("kidsSubMenu")
+kidsMenu.addEventListener("mouseover", ()=>{
+    kidsSubMenu.setAttribute("class","kidsSubMenu kidsHover")
+})
+kidsSubMenu.addEventListener("mouseover", ()=>{
+    kidsSubMenu.setAttribute("class","kidsSubMenu kidsHover")
+})
+addEventListener("mouseout", ()=>{
+    kidsSubMenu.setAttribute("class","kidsSubMenu")
+})
+const jordanMenu = document.getElementById("jordan")
+const jordanSubMenu = document.getElementById("jordanSubMenu")
+jordanMenu.addEventListener("mouseover", ()=>{
+    jordanSubMenu.setAttribute("class","jordanSubMenu jordanHover")
+})
+jordanSubMenu.addEventListener("mouseover", ()=>{
+    jordanSubMenu.setAttribute("class","jordanSubMenu jordanHover")
+})
+addEventListener("mouseout", ()=>{
+    jordanSubMenu.setAttribute("class","jordanSubMenu")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
